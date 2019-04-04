ebd8e06d4fe4523dc065ff999fb2ced9d6c7bbd1
chagnaud <chagnaud.jerome@gmail.com>
2019-04-04 12:30:07 +0200

    objet2.

--- objet2.js
+++ objet2.js
@@ -1,6 +1,6 @@
 // Création d'une "class" ES5
 
-class Personne {
+class Personne {//standard de js donc une majuscule// pour noter qu'il s'agit d'une class//
 
     constructor(nom,prenom){
         this.nom = nom;
@@ -15,3 +15,30 @@ sePresenter(){
 
 var jerome = new Personne("Chagnaud", "Jerome");
 jerome.sePresenter();
+
+//Création d'une "class" Enseignant qui hérite de la class Personne//
+
+class Enseignant extends Personne{
+  constructor(nom, prenom, diplome){
+        super(nom,prenom);
+        this.diplome = diplome;
+    //Méthodes// 
+    }
+    enseigner(){
+        console.log("J'enseigne le Javascript");
+    }
+}
+var yvan = new Enseignant("Douenel", "Yvan", "BAC");
+yvan.sePresenter();
+yvan.enseigner();
+console.log(yvan.diplome);
+
+class EnseignantProgrammation extends Enseignant{
+    enseignerMT(){
+        console.log("j'enseigne les maths");
+    }
+}
+
+var yvan = new EnseignantProgrammation ("Nom de famille", "yvan", "agregation");
+yvan.sePresenter();
+yvan.enseignerMT();
